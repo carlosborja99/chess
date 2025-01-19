d9d98e19bb38b822a72a0750dbe5cc7ca1259442
Carlos Borja <cdborja@byu.edu>
2025-01-18 22:56:42 -0700

    ChessMove changes

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -7,23 +7,29 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessMove {
+    private final ChessPosition startPosition;
+    private final ChessPosition endPosition;
+    private final ChessPiece.PieceType promotionPiece;
 
     public ChessMove(ChessPosition startPosition, ChessPosition endPosition,
                      ChessPiece.PieceType promotionPiece) {
+        this.startPosition = startPosition;
+        this.endPosition = endPosition;
+        this.promotionPiece = promotionPiece;
     }
 
     /**
      * @return ChessPosition of starting location
      */
     public ChessPosition getStartPosition() {
-        throw new RuntimeException("Not implemented");
+        return startPosition;
     }
 
     /**
      * @return ChessPosition of ending location
      */
     public ChessPosition getEndPosition() {
-        throw new RuntimeException("Not implemented");
+        return endPosition;
     }
 
     /**
@@ -33,6 +39,24 @@ public class ChessMove {
      * @return Type of piece to promote a pawn to, or null if no promotion
      */
     public ChessPiece.PieceType getPromotionPiece() {
-        throw new RuntimeException("Not implemented");
+        return promotionPiece;
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 * (31 * startPosition.hashCode() + endPosition.hashCode()) + (promotionPiece != null ? promotionPiece.hashCode() : 0);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        ChessMove movimiento = (ChessMove) obj;
+        return startPosition.equals(movimiento.startPosition) && endPosition.equals(movimiento.endPosition) && (promotionPiece == null ? movimiento.promotionPiece == null : promotionPiece.equals(movimiento.promotionPiece));
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
     }
 }
