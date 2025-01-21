d00f571729e742a54ed0fa5835483d735053e2c6
Carlos Borja <cdborja@byu.edu>
2025-01-21 10:17:38 -0700

    Changes include implementing the addPiece and getPiece methods, some of the tests require implementing methods from ChessPiece, so I'll do them later.

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,5 +1,8 @@
 package chess;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
@@ -7,14 +10,14 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessBoard {
-
+    private Map<ChessPosition, ChessPiece> board;
     public ChessBoard() {
-        
+        board = new HashMap<>();
     }
 
     @Override
     public int hashCode() {
-        return super.hashCode();
+        return board.hashCode();
     }
 
     @Override
@@ -34,7 +37,7 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        throw new RuntimeException("Not implemented");
+        board.put(position, piece);
     }
 
     /**
@@ -45,7 +48,7 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        throw new RuntimeException("Not implemented");
+        return board.get(position);
     }
 
     /**
