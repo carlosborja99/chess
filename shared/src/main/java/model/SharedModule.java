dcac222f96f8c0ef6123fa7031a69e38b08ef2ad
Carlos Borja <cdborja@byu.edu>
2025-02-19 13:03:41 -0700

    Data Model Classes

---
+++ shared/src/main/java/model/SharedModule.java
@@ -0,0 +1,10 @@
+package model;
+
+import chess.ChessGame;
+
+class SharedModule {
+    public record UserData(String username, String password, String email) { }
+    public record GameData(int gameID, String whiteUsername, String blackUsername, String gameName, ChessGame game) { }
+    public record AuthData(String authToken, String username) { }
+}
+
