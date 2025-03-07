61e6e99728fc616a3855e999646d53b8910aa92a
Carlos Borja <cdborja@byu.edu>
2025-03-07 16:01:07 -0700

    Properly added the compile part of the getting started assignment.

--- server/src/main/java/dataaccess/MySQLDataAccess.java
+++ server/src/main/java/dataaccess/MySQLDataAccess.java
@@ -1,28 +1,108 @@
 package dataaccess;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import model.AuthData;
 import model.GameData;
 import model.UserData;
-
+import org.mindrot.jbcrypt.BCrypt;
+import java.sql.*;
 import java.util.List;
 
 public class MySQLDataAccess implements DataAccess {
     private final Gson gson = new Gson();
 
+    private final String[] SQLStatement = {
+            """
+            CREATE TABLE IF NOT EXISTS users (
+                username VARCHAR (255) NOT NULL PRIMARY KEY,
+                password VARCHAR (255) NOT NULL,
+                email VARCHAR (255) NOT NULL
+            )
+            """,
+            """
+            CREATE TABLE IF NOT EXISTS games (
+                gameID INT NOT NULL PRIMARY KEY,
+                whiteUsername VARCHAR (255),
+                blackUsername VARCHAR (255),
+                gameName VARCHAR (255) NOT NULL,
+                game TEXT NOT NULL,
+                FOREIGN KEY (whiteUsername) REFERENCES user (username),
+                FOREIGN KEY (blackUsername) REFERENCES user (username)
+            )
+            """,
+            """
+            CREATE TABLE IF NOT EXISTS authorization_data (
+                authToken VARCHAR (255) NOT NULL PRIMARY KEY,
+                username VARCHAR (255) NOT NULL,
+                FOREIGN KEY (username) REFERENCES user (username)
+            )
+            """
+    };
+
+
+    public MySQLDataAccess() throws DataAccessException {
+        configureDatabase();
+    }
+
+    private void configureDatabase()  throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()) {
+            for (String statement : SQLStatement) {
+                try (var ps = conn.prepareStatement(statement)) {
+                    ps.executeUpdate();
+                }
+            }
+        }catch(SQLException e){
+            throw new DataAccessException("Unable to configure database: " + e.getMessage());
+        }
+    }
+
     @Override
     public void clear() throws DataAccessException {
-
+        String[] tables = { "games", "users", "authorization_data" };
+        try (var conn = DatabaseManager.getConnection()){
+            for (String table : tables) {
+                try (var ps = conn.prepareStatement("TRUNCATE TABLE " + table)){
+                    ps.executeUpdate();
+                }
+            }
+        }catch(SQLException e){
+            throw new DataAccessException("Unable to clear database: " + e.getMessage());
+        }
     }
 
     @Override
     public int createGameID() throws DataAccessException {
-        return 0;
+        String sql = "INSERT INTO games (gameName) VALUES (?)";
+        try (var conn = DatabaseManager.getConnection();
+            var ps = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)){
+            ps.setString(1, "New Game");
+            ps.executeUpdate();
+            try (var rs = ps.getGeneratedKeys()){
+                if (rs.next()) {
+                    return rs.getInt(1);
+                }
+                throw new DataAccessException("Unable to retrieve game ID");
+            }
+        }catch(SQLException e){
+            throw new DataAccessException("Unable to create game ID: " + e.getMessage());
+        }
     }
 
     @Override
     public void createUser(UserData user) throws DataAccessException {
-
+        String sql = "INSERT INTO users (username, password, email) VALUES (?, ?, ?)";
+        String hashedPassword = BCrypt.hashpw(user.password(), BCrypt.gensalt());
+        try (var conn = DatabaseManager.getConnection();
+            var ps = conn.prepareStatement(sql)) {
+            ps.setString(1, user.username());
+            ps.setString(2, hashedPassword);
+            ps.setString(3, user.email());
+            ps.executeUpdate();
+        } catch(SQLException e){
+            throw new DataAccessException("Unable to create user: " + e.getMessage());
+        }
     }
 
     @Override
