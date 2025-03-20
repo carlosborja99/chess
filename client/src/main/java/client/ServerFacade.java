5b80329896acebf1e9e87765814dda506ac6d837
Carlos Borja <cdborja@byu.edu>
2025-03-19 22:51:56 -0600

    completed ServerFacade.java

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -28,6 +28,23 @@ public class ServerFacade {
         return postRequest("/session", Map.of("username", username, "password", password), null);
     }
 
+    public void logout() throws Exception {
+        postRequest("/session", null, authToken);
+        authToken = null;
+    }
+
+    public Map<String, Object> createMyGame(String gameName) throws Exception {
+        return postRequest("/game", Map.of("gameName", gameName), authToken);
+    }
+
+    public List<Map<String, Object>> listOfGames() throws Exception {
+        return getThisRequest("/game", authToken);
+    }
+
+    public void joinGame(String gameID, String playerColor) throws Exception {
+        postRequest("/game", Map.of("gameID", gameID, "playerColor", playerColor), authToken);
+    }
+
     private Map<String, Object> postRequest(String serverUrl, Map<String, String> request, String authToken) throws Exception{
         URI uri = new URI(serverURL + serverUrl);
         HttpURLConnection connection = (HttpURLConnection) uri.toURL().openConnection();
@@ -37,6 +54,34 @@ public class ServerFacade {
         }
         writeRequest(connection, request);
         return response(connection);
+    }
+
+    private List<Map<String, Object>> getThisRequest(String thisEndpoint, String authToken) throws Exception {
+        URI uri = new URI(serverURL + thisEndpoint);
+        HttpURLConnection connection = (HttpURLConnection) uri.toURL().openConnection();
+        connection.setRequestMethod("GET");
+        if (authToken != null) {
+            connection.setRequestProperty("Authorization", authToken);
+        }
+        connection.connect();
+        return respondList(connection);
+    }
+
+    private List<Map<String, Object>> respondList(HttpURLConnection connection) throws Exception {
+        connection.connect();
+        int status = connection.getResponseCode();
+        if (status >= 200 && status < 300) {
+            try (var in = connection.getInputStream()) {
+                Type listType = new TypeToken<List<Map<String, Object>>>() {}.getType();
+                Map<String, List<Map<String, Object>>> response = new Gson().fromJson(new InputStreamReader(in), listType);
+                return response.get("games");
+            }
+        } else {
+            try (var in = connection.getErrorStream()) {
+                String error = new Gson().fromJson(new InputStreamReader(in), Map.class).get("message").toString();
+                throw new Exception(error);
+            }
+        }
     }
 
     private void writeRequest(HttpURLConnection connection, Map<String, String> request) throws Exception{
@@ -67,5 +112,7 @@ public class ServerFacade {
         }
     }
 
-
+    public String getAuthToken() {
+        return authToken;
+    }
 }
