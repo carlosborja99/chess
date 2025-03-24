43ef25be737bf65cc662edfb70681919c14642f2
Carlos Borja <cdborja@byu.edu>
2025-03-23 18:22:16 -0600

    Added login commands

--- client/src/main/java/client/Repl.java
+++ client/src/main/java/client/Repl.java
@@ -31,6 +31,44 @@ public class Repl {
             }
         }
     }
+    private void preLogin() throws Exception{
+        System.out.print(">>> ");
+        String[] input = scanner.nextLine().split("\\s+");
+        String command = input[0].toLowerCase();
 
+        switch (command){
+            case "help":
+                System.out.println("Available commands:");
+                System.out.println("  help - Display this help text");
+                System.out.println("  quit - Exit the program");
+                System.out.println("  login <username> <password> - Log in to your account");
+                System.out.println("  register <username> <password> <email> - Create a new account");
+            case "quit":
+                System.out.println("Goodbye!");
+                System.exit(0);
+                break;
+            case "register":
+                if(input.length != 4){
+                    System.out.println("Usage: register <username> <password> <email>");
+                }
+                Map<String, Object> registerResponse = facade.register(input[1], input[2], input[3]);
+                System.out.println("Registered and logged in as " + input[1]);
+                logged = true;
+                break;
+            case "login":
+                if(input.length != 3){
+                    System.out.println("Usage: login <username> <password>");
+                    break;
+                }
+                Map<String, Object> loginResponse = facade.login(input[1], input[2]);
+                System.out.println("Logged in as " + input[1]);
+                logged = true;
+                break;
+            case "logout":
+                break;
+            default:
+                System.out.println("Unknown command. Type \"help\" for options.");
+        }
+    }
 
 }
