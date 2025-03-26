a0e3858e54be383885734f94638731c4e17e4637
Carlos Borja <cdborja@byu.edu>
2025-03-25 19:35:42 -0600

    Added second method to tests that now helps them run successfully

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,9 +1,12 @@
 package client;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import org.junit.jupiter.api.*;
 import server.Server;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URI;
 import java.util.List;
@@ -44,6 +47,23 @@ public class ServerFacadeTests {
         return connect;
     }
 
+    private Map<String, Object> getResponse(HttpURLConnection connection) throws Exception {
+        int status = connection.getResponseCode();
+        if (status >= 200 && status < 300) {
+            try (var in = connection.getInputStream()) {
+                if (in.available() == 0) {
+                    return Map.of();
+                }
+                return new Gson().fromJson(new InputStreamReader(in), new TypeToken<Map<String, Object>>(){}.getType());
+            }
+        } else {
+            try (var in = connection.getErrorStream()) {
+                String error = new Gson().fromJson(new InputStreamReader(in), Map.class).get("message").toString();
+                throw new Exception(error);
+            }
+        }
+    }
+
     @Test
     void registerSuccess() throws Exception {
         Map<String, Object> authData = facade.register("player", "password", "player@email.com");
@@ -51,7 +71,6 @@ public class ServerFacadeTests {
         assertTrue(((String) authData.get("authToken")).length() > 10);
         assertEquals("player", authData.get("username"));
     }
-
-
+    
 
 }
